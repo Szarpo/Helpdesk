9b146abc512ca516602164fb015e413bfe3630d5
Paweł Wiącek <97812236+Szarpo@users.noreply.github.com>
2024-02-06 20:54:45 +0100

    Refactor Dto

--- src/Helpdesk.Application/DTO/CommentsDto.cs
+++ src/Helpdesk.Application/DTO/CommentsDto.cs
@@ -1,8 +1,8 @@
 namespace Helpdesk.Application.DTO;
 
 public record CommentsDto
-{
-    public Guid UserId { get;  set; }
-    public string Content { get;  set; }
-    public DateTime CreatedAt { get;  set; }
-}
+(
+     Guid UserId,
+     string Content,
+     DateTime CreatedAt
+);
