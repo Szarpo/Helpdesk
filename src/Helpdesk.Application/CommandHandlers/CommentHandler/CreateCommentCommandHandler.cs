1ff82a3d3a7ba38441d74caf953cfb9adc8f0833
Paweł Wiącek <97812236+Szarpo@users.noreply.github.com>
2024-02-14 22:50:40 +0100

    Add creatorId by HttpAccessor

--- src/Helpdesk.Application/CommandHandlers/CommentHandler/CreateCommentCommandHandler.cs
+++ src/Helpdesk.Application/CommandHandlers/CommentHandler/CreateCommentCommandHandler.cs
@@ -1,8 +1,10 @@
+using System.Security.Authentication;
 using Helpdesk.Application.Commands.CommentCommand;
 using Helpdesk.Core.Abstractions;
 using Helpdesk.Core.Entities;
 using Helpdesk.Core.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace Helpdesk.Application.CommandHandlers.CommentHandler;
 
@@ -11,22 +13,32 @@ public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommand>
 
     private readonly ICommentRepository _commentRepository;
     private readonly IClock _clock;
+    private readonly IHttpContextAccessor _contextAccessor;
 
-    public CreateCommentCommandHandler(ICommentRepository commentRepository, IClock clock)
+    public CreateCommentCommandHandler(ICommentRepository commentRepository, IClock clock, IHttpContextAccessor contextAccessor)
     {
         _commentRepository = commentRepository;
         _clock = clock;
+        _contextAccessor = contextAccessor;
     }
     
     public async Task Handle(CreateCommentCommand request, CancellationToken cancellationToken)
     {
+
+        if (_contextAccessor.HttpContext.User.Identity!.Name is null)
+        {
+            throw new InvalidCredentialException();
+        }
+
+        var creatorId = Guid.Parse(_contextAccessor.HttpContext.User.Identity.Name);
+        
         var currentTime =  _clock.Current().AddHours(1);
         
-        var (ticketId, userId, content) = request;
+        var (ticketId, content) = request;
 
         var comment = Comment.Create(
             ticketId: ticketId,
-            userId: userId,
+            userId: creatorId,
             content: content,
             createdAt: currentTime
             );
--- src/Helpdesk.Application/CommandHandlers/TicketHandler/CreateTicketCommandHandler.cs
+++ src/Helpdesk.Application/CommandHandlers/TicketHandler/CreateTicketCommandHandler.cs
@@ -1,7 +1,9 @@
+using System.Security.Authentication;
 using Helpdesk.Application.Commands.TicketCommand;
 using Helpdesk.Core.Abstractions;
 using Helpdesk.Core.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace Helpdesk.Application.CommandHandlers.TicketHandler;
 
@@ -10,21 +12,31 @@ public class CreateTicketCommandHandler : IRequestHandler<CreateTicketCommand>
 
     private readonly ITicketRepository _ticketRepository;
     private readonly IClock _clock;
+    private readonly IHttpContextAccessor _contextAccessor;
+    
 
-    public CreateTicketCommandHandler(ITicketRepository ticketRepository, IClock clock)
+    public CreateTicketCommandHandler(ITicketRepository ticketRepository, IClock clock, IHttpContextAccessor contextAccessor)
     {
         _ticketRepository = ticketRepository;
         _clock = clock;
+        _contextAccessor = contextAccessor;
     }
     
     public async Task Handle(CreateTicketCommand request, CancellationToken cancellationToken)
     {
-        var (creatorId, title, content,category) = request;
+        if (_contextAccessor.HttpContext.User.Identity!.Name is null)
+        {
+            throw new InvalidCredentialException();
+        }
+
+        var userId =  Guid.Parse(_contextAccessor.HttpContext.User.Identity.Name);
+        
+        var (title, content,category) = request;
 
         var currentTime = _clock.Current().AddHours(1);
         
         var createTicket = Core.Entities.Ticket.Create(
-            creatorId: creatorId,
+            creatorId: userId,
             title: title,
             content: content,
             category: category,
